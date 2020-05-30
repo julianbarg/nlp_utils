290a9ef33c0c6c9e3023ca1e95198c5cfbe73371
julianbarg <barg.julian@gmail.com>
2020-05-30 16:49:34 -0400

    Added comments.

--- pdf_contents.py
+++ pdf_contents.py
@@ -4,15 +4,19 @@ from os import listdir
 
 folder = '/home/julian/Documents/Downloaded Articles'
 
+# Read in the pdfs
 pdfs = listdir(folder)
 texts = [str(textract.process(folder + "/" + pdf)) for pdf in pdfs]
 lengths = [len(text) for text in texts]
 dict = {'name': pdfs, 'text': texts, 'length': lengths}
-
 df = pd.DataFrame(dict)
-df['attention'] = df.text.str.count('attention')
 
+# Cleaning
 df['name'] = df.name.str.replace(';', ',')
 
+# Count keywords
+df['attention'] = df.text.str.count('attention')
+
+# Save output
 df.to_csv('output.csv')
 df.to_csv('output_small.csv', columns=['name', 'length', 'attention'])
